b8b0a0047841334e9e7e2d5073cc9a086ba3f681
Macbook Pro <macbookpro@MacBook-Pro-Macbook.local>
2019-11-19 16:06:34 +0200

    sol

---
+++ less2/hw1/index.js
@@ -0,0 +1,11 @@
+let a = 4;
+let b = 2;
+
+
+console.log('a: ', 4);
+console.log('b: ', 2);
+
+console.log('a - b = ', a - b);
+console.log('a + b = ', a + b);
+console.log('a / b = ', a / b);
+console.log('a * b = ', a * b);
