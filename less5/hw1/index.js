2ea41aaae65a7958c8678867ba616cd940ec7925
Macbook Pro <nastyarosl@gmail.com>
2019-11-22 00:35:57 +0200

    sol

---
+++ less5/hw1/index.js
@@ -0,0 +1,9 @@
+function getSum (firstNum, secondNum){
+   let result = 0;
+    for (let i = firstNum; i <= secondNum; i++) {
+        if (i % 2 === 0) {
+            result += i;
+        }
+    }
+    return(result);
+}
