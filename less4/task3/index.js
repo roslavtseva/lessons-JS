354b58ecb5732d2b645aac2c4c8adca02f5afc21
Macbook Pro <nastyarosl@gmail.com>
2019-11-21 15:21:04 +0200

    sol

--- less4/task3/index.js
+++ less4/task3/index.js
@@ -1,6 +1,6 @@
-for(i = 1; i <= 5; i++){
+for(let i = 1; i <= 5; i++){
     console.log('\n=> ' + i + ' <=\n\n')
-    for(k = 1; k <= 10; k++){
+    for(let k = 1; k <= 10; k++){
         console.log( i + ' x ' + k + ' = ' + i * k);
     }
 }
