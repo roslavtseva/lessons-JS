2625b32e41c1c18d3df1e74cb9b960433741ef95
Macbook Pro <nastyarosl@gmail.com>
2019-11-21 17:02:39 +0200

    sol

---
+++ less4/hw3/index.js
@@ -0,0 +1,15 @@
+let sum = 0;
+
+for (let i = 0; i <= 1000; i++) {
+    if (i % 2 !== 0){
+        console.log('Found');
+        sum +=i;
+    }
+    if (sum * 5 > 5000){
+        console.log('Bigger');
+    }
+    else if (sum * 5 < 5000){
+        console.log('Smaller or equal');
+    }
+
+}
