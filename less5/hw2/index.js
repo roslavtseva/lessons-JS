ab65b69730cc62e187fa495735a5e808ab02e37f
Macbook Pro <nastyarosl@gmail.com>
2019-11-22 11:53:52 +0200

    sol

--- less5/hw2/index.js
+++ less5/hw2/index.js
@@ -1,11 +1,12 @@
 let n = 10;
 
 getPrimes:
-for (let i = 2; i <= n; i++) { // Для всiх i...
+for (let i = 1; i <= n; i++) { 
 
-  for (let h = 2; h < i; h++) { // провірити чи ділиться число
-    if (i % h == 0) continue getPrimes; // якщо не підходить берем наступне
+  for (let h = 2; h < i; h++) { 
+    if (i % h == 0) continue getPrimes; 
   }
 
-  console.log( i ); // просте число
+  console.log(i); 
 }
+
