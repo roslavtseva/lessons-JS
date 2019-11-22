229292cd1bbf9bd85ccebcc831df56471e7fd041
Macbook Pro <nastyarosl@gmail.com>
2019-11-22 13:11:05 +0200

    sol

--- less5/hw2/index.js
+++ less5/hw2/index.js
@@ -1,26 +1,8 @@
-/*let n = 10;
-
+let n = 10;
 getPrimes:
 for (let i = 1; i <= n; i++) { 
-
   for (let h = 2; h < i; h++) { 
     if (i % h == 0) continue getPrimes; 
   }
-
   console.log(i); 
-}*/
-
-let n = 10;
-
-function getPrimes(){
-for (let i = 1; i <= n; i++) { 
-  if (anotherPrimes(i)) console.log(i); 
 }
-}
-
-function anotherPrimes(){
-  for (let h = 2; h < i; h++) { 
-    if (i % h == 0) continue getPrimes; 
-  }
-}
-
