2eadc5fdd1336d3b023baea2324fd634762b2cd1
Macbook Pro <nastyarosl@gmail.com>
2019-11-22 14:52:21 +0200

    sol

--- less5/hw4/index.js
+++ less5/hw4/index.js
@@ -1,17 +1,16 @@
 function compareSums(a, b, c, d) {
-    if (sum(a + b) > sum(c + d)){
-        return (true);
+    if (sum(a, b) > sum(c, d)) {
+        return true;
     } else {
-        return (false);
+        return false;
     }
 }
 
-function sum (from, to){
+function sum(from, to) {
     let result = 0;
-    for (let x = from; x <= to; x++){
-        result += x;
+    for (let i = from; i <= to; i++) {
+        result += i;
     }
-
     return result;
 }
 
