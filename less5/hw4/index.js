e8a2f28990bbd547af77aa2eaeeec241de4ea463
Macbook Pro <nastyarosl@gmail.com>
2019-11-22 14:47:39 +0200

    sol

--- less5/hw4/index.js
+++ less5/hw4/index.js
@@ -1,4 +1,4 @@
-/*function compareSums(a, b, c, d) {
+function compareSums(a, b, c, d) {
     if (sum(a + b) > sum(c + d)){
         console.log (true);
     } else {
@@ -11,23 +11,7 @@ function sum (from, to){
     for (x = from; x <= to; x++){
         result += x;
     }
-    
-    return result;
-}*/
 
-function compareSums(a, b, c, d) {
-
-    if ((a + b) > (c + d)){
-        console.log(true);
-    } else {
-        console.log (false);
-    }
-    return sum(a, b, c, d);
-}
-function sum (from, to){
-    let x = 0;
-    for (x = from; x <= to; x++){
-        result += x;
-    }
     return result;
 }
+
