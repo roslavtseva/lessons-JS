0085c1f6e03219a06d24682994594826b0c2ff1b
Macbook Pro <nastyarosl@gmail.com>
2019-11-23 18:42:18 +0200

    sol

--- less6/task3/index.js
+++ less6/task3/index.js
@@ -1,12 +1,14 @@
-function getSpecialNumbers(m,n){
+function getSpecialNumbers(m, n) {
     let specialNumbers = [];
 
-    for (let i = m; i <= n; i++);{
-        if (i % 3 == 0){
 
-            specialNumbers.pop(i);
+    for (let i = m; i <= n; i++); {
+        if (i % 3 == 0) {
+            specialNumbers.push(i);
         }
+
     }
 
     return specialNumbers;
+
 }
