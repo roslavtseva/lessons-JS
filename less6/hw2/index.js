596df46b3370bac984bdb693dc1527adbea529ca
Macbook Pro <nastyarosl@gmail.com>
2019-11-23 20:05:11 +0200

    sol

--- less6/hw2/index.js
+++ less6/hw2/index.js
@@ -3,10 +3,7 @@ function reverseArray(arr) {
     if (!Array.isArray(arr)) {
         return null;
     }
-    const x = [];
-    for (let i of arr) {
-        x.slice(i).reverse(i);
-    }
+    const x = arr.slice().reverse();
     return x;
 }
 
