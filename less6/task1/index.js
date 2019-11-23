cc345e7740c77c0641a1b584fdf281e028081e42
Macbook Pro <nastyarosl@gmail.com>
2019-11-23 17:41:44 +0200

    sol

--- less6/task1/index.js
+++ less6/task1/index.js
@@ -1,7 +1,5 @@
-function getArrayBounds(arr){
-    if (!Array.isArray[arr]){
-        return null;
-    }
-    return[arr.length, arr[0], arr[arr.length - 1]];
 
+function getArrayBounds(arr) {
+    if (!Array.isArray(arr)) return null;
+    return [arr.length, arr[0], arr[arr.length - 1]];
 }
