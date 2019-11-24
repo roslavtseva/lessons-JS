a7689def11be03e6537bb0bc0c2c5449f301ec6f
Macbook Pro <nastyarosl@gmail.com>
2019-11-24 19:14:16 +0200

    sol

--- less6/hw7/index.js
+++ less6/hw7/index.js
@@ -2,17 +2,19 @@ function checker(arr) {
     let min = arr[0];
     let max = arr[0];
 
-  for (let x = 0; x < arr.length; x++){
-      if (arr[x] > max){
-          max = array[x];
-      }
-      if (arr[x] < min) {
-          min = array[x];
-      }
-  }
-  if (min + max > 1000){
-      return true;
-  } return false;
+    for (let x = 0; x < arr.length; x++) {
+        if (arr[x] > max) {
+            max = array[x];
+        }
+        if (arr[x] < min) {
+            min = array[x];
+        }
+    }
+    if (min + max > 1000) {
+        return true;
+    } else { 
+        return false; 
+    }
 }
 
 
