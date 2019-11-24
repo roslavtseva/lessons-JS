5b4fe869882345a0d7cc903276a49cf80bb193aa
Macbook Pro <nastyarosl@gmail.com>
2019-11-24 19:11:29 +0200

    sol

---
+++ less6/hw7/index.js
@@ -0,0 +1,22 @@
+function checker(arr) {
+    let min = arr[0];
+    let max = arr[0];
+
+  for (let x = 0; x < arr.length; x++){
+      if (arr[x] > max){
+          max = array[x];
+      }
+      if (arr[x] < min) {
+          min = array[x];
+      }
+  }
+  if ((min + max) > 1000){
+      return true;
+  } return false;
+}
+
+
+
+/**Напишите функцию checker(arr), которая будет суммировать макс и мин элементы массив, и возвращать true если сумма больше 1000, и false в другом случае
+
+ */
