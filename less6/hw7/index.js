f14e4dd4e3205687c848a3c27fc7e8b4b3ac057c
Macbook Pro <nastyarosl@gmail.com>
2019-11-24 19:29:38 +0200

    sol

--- less6/hw7/index.js
+++ less6/hw7/index.js
@@ -1,21 +1,22 @@
 function checker(arr) {
-    let min = arr[0];
+    let min = arr[0]; //берем перший елемент масива
     let max = arr[0];
 
-    for (let x = 0; x < arr.length; x++) {
-        if (arr[x] > max) {
-            max = array[x];
+    for (let x = 0; x < arr.length; x++) {//перебор
+        if (arr[x] > max) { //якщо елемент більший ніж в змінній
+            max = array[x]; //присвоюєм його значення змінній
         }
-        if (arr[x] < min) {
+        if (arr[x] < min) { //так само але з меншим
             min = array[x];
         }
     };
     if (min + max > 1000) {
         return true;
-    } else { 
-        return false; 
+    } else {
+        return false;
     }
 };
+    
 
 
 
