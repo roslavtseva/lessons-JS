f34849dd4a80142f95f3aaedfbc3bc96af163299
Macbook Pro <nastyarosl@gmail.com>
2019-11-26 13:44:13 +0200

    sol

---
+++ less9/hw1/index.js
@@ -0,0 +1,65 @@
+
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17
+    },
+    
+};
+
+const getCustomersList = customersObj => {
+    const customersArr = Object.entries(customersObj); //переводим в масив масивів
+
+    const customersNames = customersArr 
+        .map[customer => customer[0]];
+
+};
+
+console.log()
+
+
+
+
+
+
+
+
+/*Данные о пользвателях храняться в виде объекта в формате
+
+    const customers = {
+        'customer-id-1': {
+            name: 'William',
+            age: 54
+        },
+        'customer-id-2': {
+            name: 'Tom',
+            age: 17
+        },
+        ...
+    };
+Нужно написать функцию, которая такой объект преобразует в массив и отсортирует пользователей по возрасту от самого младшего до старшего
+
+Основные требования:
+Функция с именем getCustomersList должна принимать объект в заданом выше формате
+Функция должна вернуть список пользователей в виде массива, отсортированного по возрасту по возрастанию.
+Элементы итогового массива должны включать поле id, значением которого является ключ из исходного объекта
+            [
+                {
+                    name: 'Tom',
+                    age: 17,
+                    id: 'customer-id-2'
+                },     
+                {
+                    name: 'William',
+                    age: 54,
+                    id: 'customer-id-1'
+                },
+                ...
+            ]
+        
+Исходный объект должен остаться неизменным
+*/
