3fdf81f5af4862043b6d9ba7c90dc9b18f906515
Macbook Pro <nastyarosl@gmail.com>
2019-12-02 15:53:02 +0200

    sol

--- less15/task1/index.js
+++ less15/task1/index.js
@@ -5,13 +5,7 @@ return function(){
 }
 };
 
-const counter1 = makeCounter();
-const counter2 = makeCounter();
-console.log(counter1()); // 0
-console.log(counter1()); // 1
-console.log(counter2()); // 0
-console.log(counter1()); // 2
-console.log(counter2()); // 1
+
 
 /**Создайте ф-цию, которая будет создавать независимые счетчики
 
