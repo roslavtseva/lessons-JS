a2bf4318e671810f19281bcc44e5c9a01c4e7973
Macbook Pro <nastyarosl@gmail.com>
2019-12-02 15:51:34 +0200

    sol

---
+++ less15/task1/index.js
@@ -0,0 +1,33 @@
+const makeCounter = () => {
+    let count = 0;
+return function(){
+    return count++;
+}
+};
+
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+console.log(counter1()); // 0
+console.log(counter1()); // 1
+console.log(counter2()); // 0
+console.log(counter1()); // 2
+console.log(counter2()); // 1
+
+/**Создайте ф-цию, которая будет создавать независимые счетчики
+
+Основные требования:
+Функция должна называться makeCounter и экспортироваться под этим именем из файла
+Вызов ф-ции makeCounter возвращает ф-цию счетчик. Счетчик возвращает инткремент на каждом вызове (начиная с 0).
+Счетчики, созданные с помощью makeCounter должны быть независимы друг от друга
+Пример работы:
+            const counter1 = makeCounter();
+            const counter2 = makeCounter();
+            console.log(counter1()); // 0
+            console.log(counter1()); // 1
+            console.log(counter2()); // 0
+            console.log(counter1()); // 2
+            console.log(counter2()); // 1
+
+Обязательно покрывайте и отлаживайте ваше решение тестами перед проверкой на сайте
+
+ */
