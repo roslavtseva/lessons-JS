9669a000c4148fe4b1bad0f65a8ecd6308047378
Macbook Pro <nastyarosl@gmail.com>
2019-12-02 14:45:21 +0200

    sol

---
+++ less14/task2/index.js
@@ -0,0 +1,45 @@
+
+
+
+const createMessenger = () => {
+    
+    let sender = 'Gromcode';
+    let message = 'Good job!';
+
+    function sendMessage(name) {
+        console.log(`${name}, ${message} Your ${sender}`);
+    }
+
+    function setMessage(text) {
+        message = text;
+    }
+
+    function setSender(text) {
+        message = text;
+    }
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    };
+};
+//export default createMessenger();
+
+//messenger.sendMessage('Bob');
+
+
+
+
+
+
+/**Создайте ф-цию, которая будет создавать независимые счетчики
+
+Основные требования:
+Файл должен содежать ф-цию createMessenger, создает объект с 3-мя методами:
+sendMessage - принимает имя и выводит в консоль сообщение в формате 'Bob, Just learn it! Your Gromcode', где 'Bob' - введенное имя
+setMessage - принимает текст, который дальше метод sendMessage будет использовать в результате вывода в консоль 'Bob, Good job! Your Gromcode' (setMessage устанавливает текст, выделенный жирным)
+setSender - принимает текст, который дальше метод sendMessage будет использовать в результате вывода в консоль 'Bob, Just learn it! Your Gromcode' (setSender устанавливает текст, выделенный жирным)
+createMessenger должен экспортироваться из файл по умолчанию
+Созданные с помощью ф-ции createMessenger месседжеры должны быть независимы друг от друга
+ */
