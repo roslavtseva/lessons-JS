ab781b01caffa4b8208fbcaee1570f341f2cf54a
Macbook Pro <nastyarosl@gmail.com>
2019-12-03 15:34:39 +0200

    sol

--- less16/hw1/index.js
+++ less16/hw1/index.js
@@ -2,7 +2,7 @@
 function createArrayOfFunctions(number) {
 
     if (typeof number !== 'number') return null;
-    if (number === undefined) return null;
+    if (number === undefined) return [];
     let arr = [];
     for (let i = 0; i < number; i++) {
         arr[i] = function () {
