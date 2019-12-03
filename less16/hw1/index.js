aeb959e213eb3762a1ec45b62aaad38302bfdf6a
Macbook Pro <nastyarosl@gmail.com>
2019-12-03 13:55:52 +0200

    sol

---
+++ less16/hw1/index.js
@@ -0,0 +1,26 @@
+
+function createArrayOfFunctions(number) {
+
+    if (typeof number !== 'number') return null;
+    if (number === undefined) return null;
+    let arr = [];
+    for (let i = 0; i < number; i++) {
+        arr[i] = function () {
+            return i;
+        }
+    }
+    return Array;
+};
+
+console.log(createArrayOfFunctions());
+
+//export { createArrayOfFunctions };
+/**Создайте фабрику массивов ф-ций :)
+
+Основные требования:
+Файл index.js должен експортировать ф-цию под именем createArrayOfFunctions
+Ф-ция createArrayOfFunctions должна принимать целое число, как единственный аргумент. Это число укажет размерность результирующего массива
+Ф-ция должно вернуть массив заданной размерности, каждым элементом которого будет ф-ция, которая возвращает свой индекс в массиве
+Пример работы: createArrayOfFunctions(9)[5](); // 5
+Если передано не число, нужно вернуть null
+Если аргумент не передан, то вернуть пустой массив*/
