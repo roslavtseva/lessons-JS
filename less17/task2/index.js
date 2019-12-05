d1eb813aff83df5a7903429b2aa7a6d2cabf04da
Macbook Pro <nastyarosl@gmail.com>
2019-12-05 14:43:49 +0200

    sol

---
+++ less17/task2/index.js
@@ -0,0 +1,20 @@
+const callbackPrompt = {
+    message: 'Tell me your number',
+    showPrompt() {
+       const phoneNumber =  prompt(this.message);// вспливаюче вікно з запросом 
+       console.log(phoneNumber); //передача інфи в консоль
+    },
+    showDeferredPrompt(ms) {
+        setTimeout(this.showPrompt.bind(this), ms);
+    }
+};
+callbackPrompt.showPrompt();
+
+
+/**Создайте объект, который будет запрашивать у пользователя номер телефона для обратного звонка
+
+Основные требования:
+Экспортируйте из файла объект с именем callbackPrompt
+Объект callbackPrompt должен хранить сообщение в свойстве message
+Метод showPrompt должен содержать сообщение из свойства message и запросить у пользователя номер телефона, который потом выводит в консоль
+Метод showDeferredPrompt должен вызывать метод showPrompt через указонное количество миллисекунд, переданное как единственный аргумент */
