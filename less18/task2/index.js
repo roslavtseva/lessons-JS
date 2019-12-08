dd5964a8d30cd6ddc14e014b1c9164444756c8a4
Macbook Pro <nastyarosl@gmail.com>
2019-12-08 16:40:27 +0200

    sol

---
+++ less18/task2/index.js
@@ -0,0 +1,24 @@
+const wallet ={
+    transactions: [2, 6, 77, 8, 13, 768, 100],
+    getMax() {
+            return Math.max.apply(null, this.transactions); // null- це  просто змінник назви контексту
+    },
+    getMin() {
+            return Math.min.apply(null, this.transactions);
+    },
+
+};
+//console.log(wallet.getMin());
+//console.log(wallet.getMax());
+export {wallet};
+
+
+
+
+/** Опишите объект кошелька, который будет содержать информацию о транзакциях, и возможность возвращать максимальную и минимальную транзакцию
+
+Основные требования:
+Экспортируйте из файла объект с именем wallet
+Объект wallet должен хранить массив транзакций в свойстве transactions
+Метод getMax должен вернуть значение максимальной транзакции
+Метод getMin должен вернуть значение минимальной транзакции */
