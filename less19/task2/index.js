90b568d0f541a56c8d3f82ea7105bb27dec2dbac
Macbook Pro <nastyarosl@gmail.com>
2019-12-09 12:51:45 +0200

    sol

--- less19/task2/index.js
+++ less19/task2/index.js
@@ -3,7 +3,30 @@
 Основные требования:
 Функция должна экспортироваться под именем getOwnProps
 Функция должна принимать объект, как единственный аргумент и вернуть массив собственных свойств объекта. Методы в результат попасть не должны
- */
+ const vehicle = {
+    move() {
+        console.log(`${this.name} is moving`);
+    },
+    stop() {
+        console.log(`${this.name} stopped`);
+
+    },
+};
+//vehicle.stop();
+
+
+const ship = {
+    name: 'Argo',
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+        ship.move();
+    },
+    stopMachine() {
+        console.log(`${this.name} lifting anchor down`);
+        ship.stop();
+    },
+};
+Object.setPrototypeOf(ship, vehicle);*/
 
  function getOwnProps(obj) {
      let arrForProps = [];
@@ -14,5 +37,5 @@
      }
      return arrForProps;
  };
-
+console.log(getOwnProps(ship));
  export { getOwnProps }; 
