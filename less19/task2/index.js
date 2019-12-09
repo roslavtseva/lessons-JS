120337e246e2e8ca43aba3debc239e36b5f0d539
Macbook Pro <nastyarosl@gmail.com>
2019-12-09 12:45:32 +0200

    sol

--- less19/task2/index.js
+++ less19/task2/index.js
@@ -5,4 +5,14 @@
 Функция должна принимать объект, как единственный аргумент и вернуть массив собственных свойств объекта. Методы в результат попасть не должны
  */
 
- 
+ function getOwnProps(obj) {
+     let arrForProps = [];
+     for (let prop in obj){
+         if (obj.hasOwnProperty(prop) && typeof(obj[prop]) !== 'function'){
+                arrForProps.push(prop);
+         }
+     }
+     return arrForProps;
+ };
+
+ export { getOwnProps }; 
