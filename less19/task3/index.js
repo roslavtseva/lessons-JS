6efbb2f04015f054b59ba82908546bcb268f19b4
Macbook Pro <nastyarosl@gmail.com>
2019-12-09 13:11:05 +0200

    sol

---
+++ less19/task3/index.js
@@ -0,0 +1,26 @@
+
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    get fullName(){
+        return`${this.firstName} ${this.lastName}`;
+    },
+    set fullName(value){
+        const [firstName, lastName] = value.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
+    },
+};
+user.fullName = 'John Doe';
+console.log(user.firstName);
+
+
+
+
+/**Создайте класс пользователя
+
+Основные требования:
+Объект пользователя должен экспортироваться по умолчанию
+В объекте пользователя храняться свойства firstName и lastName
+Getter fullName должен вернуть строку вида John Doe, где John - это свойство firstName, а Doe - это свойство lastName
+Setter fullName должен принять значение в формате John Doe, где John - это свойство firstName, а Doe - это свойство lastName и сохранить значения в соответствующие свойства объекта */
