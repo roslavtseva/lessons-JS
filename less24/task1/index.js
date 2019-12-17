d0c1258ef72c960397e74c1aaf9eff480c44c5b4
Macbook Pro <nastyarosl@gmail.com>
2019-12-17 11:27:47 +0200

    sol

--- less24/task1/index.js
+++ less24/task1/index.js
@@ -1,7 +1,7 @@
 
 const weekDays = [  'Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su']
 
-const dayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
 
