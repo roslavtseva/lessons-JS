6b17d68095c080da0eb4acf23cd66b788868b52e
Macbook Pro <nastyarosl@gmail.com>
2019-12-17 11:31:33 +0200

    sol

--- less24/task1/index.js
+++ less24/task1/index.js
@@ -1,5 +1,5 @@
 
-const weekDays = [  'Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su']
+const weekDays = [ 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St', ]
 
 const dayOfWeek = (date, days) => {
     const day = new Date(date).getDate();
