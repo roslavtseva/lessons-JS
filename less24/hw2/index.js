c8744cf0af27ef162d939ba4235f2bf554180db8
Macbook Pro <nastyarosl@gmail.com>
2019-12-17 16:12:52 +0200

    sol

--- less24/hw2/index.js
+++ less24/hw2/index.js
@@ -1,6 +1,45 @@
+const students = [
+{
+    name: 'Tom', birthDate: '01/15/2010',
+},
+{
+    name: 'Ann', birthDate: '02/17/2011',
+},
+{
+    name: 'Illya', birthDate: '03/15/2006',
+},
+{
+    name: 'Kate', birthDate: '01/14/2010',
+},
+{
+    name: 'Oleg', birthDate: '03/15/2010',
+},
+];
+
+const months = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+];
+
+function studentsBirthDays(students) {
+    let monthsObj = {};
+
+    students
+    .sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
+    .map(x => {
+        if (monthsObj[months[new Date(x.birthDate).getMonth()]] === undefined) {
+            monthsObj[months[new Date(x.birthDate).getMonth()]] = [];
+        }
+        monthsObj[months[new Date(x.birthDate).getMonth()]].push(x.name);
+    });
+    return monthsObj;
+
+};
+
+console.log(studentsBirthDays(students));
 
 
 
+export { studentsBirthDays };
 
 
 
