a2762ba297b0907259e487f726b4b761aa1dd2f0
Macbook Pro <nastyarosl@gmail.com>
2019-12-17 11:23:05 +0200

    sol

---
+++ less24/task1/index.js
@@ -0,0 +1,28 @@
+
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+const getdayOfWeek = (date, days) => {
+    const day = new Date(date).getDate();
+    const dateInFuture = new Date(date).setDate(day + days);
+
+    return weekDays[new Date(dateInFuture).getDay()];
+};
+
+const result = getdayOfWeek(new Date(2019, 0, 1), 14);
+console.log(result);
+
+
+
+
+
+
+
+
+/*Напишите функцию, которая выведет день недели через n дней после указанной даты.
+
+Основные требования:
+Функция должна экспортироваться под именем dayOfWeek
+Сигнатура: dayOfWeek(date, days), где date - объект даты, days - целое число дней.
+Функция должна вернуть строку вида 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'
+Считаем что неделя стартует с воскресенья
+Входящая дата должна остаться неизменной*/
