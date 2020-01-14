bfbc78095ad22765333df67d9c1c3d9b1cd2c134
Macbook Pro <nastyarosl@gmail.com>
2020-01-14 13:49:01 +0200

    sol

---
+++ less27/task1/index.js
@@ -0,0 +1,15 @@
+  
+function getLocalStorageData() {
+    return Object.entries(localStorage)
+        .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                newValue = JSON.parse(value);
+            } catch (error) {
+                newValue = value;
+            }
+            return {...acc, [key]: newValue,};
+        }, {});
+};
+
+export { getLocalStorageData };
