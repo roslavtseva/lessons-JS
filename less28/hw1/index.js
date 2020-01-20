03fb648cdd4a59e2459e5df7f8e4090e27ac4e10
Macbook Pro <nastyarosl@gmail.com>
2020-01-20 17:30:54 +0200

    sol

--- less28/hw1/index.js
+++ less28/hw1/index.js
@@ -1,48 +1,42 @@
-
-function shmoment(date) {
-    const result = new Date(date);
-
-    const functions = {
-        years: (value) => result.setFullYear(result.getFullYear() + value),
-        months: (value) => result.setMonth(result.getMonth() + value),
-        days: (value) => result.setDate(result.getDate() + value),
-        hours: (value) => result.setHours(result.getHours() + value),
-        minutes: (value) => result.setMinutes(result.getMinutes() + value),
-        seconds: (value) => result.setSeconds(result.getSeconds() + value),
-        milliseconds: (value) => result.setMilliseconds(result.getMilliseconds() + value),
-    };
-
-    return {
-        add(period, value) {
-            functions[period](value);
-            return this;
-        },
-        subtract(period, value) {
-            functions[period](-value);
-            return this;
-        },
-        result() {
-            return new Date(result);
-        },
-    };
-};
-
-export { shmoment };
-
- //console.log(new Date(2020, 0, 7, 17, 17, 17))
- //console.log(shmoment(new Date(2020, 0, 7, 17, 17, 17)).add('minutes', 6).add('days', 11).subtract('years', 4).result())
-
-
-
-
-/**Напишите функцию, которая в цепочке вызовов проводит операции с входной датой
+/**Найти наибольшее число Фибоначчи, которое не больше заданного
 
 Основные требования:
-Функция shmoment должна принять начальное значение и вернуть объект с набором методов
-Метод result должен вернуть дату после всех преобразований
-Должна быть поддержка прибавления .add('days', 5)
-Должна быть поддержка вычитания .subtract('minutes', 78)
-Прибавить и вычитать можно 'years', 'months', 'days', 'hours', 'minutes', 'seconds', 'milliseconds'
-Должна быть поддержка вызывать методы add, subtract в цепочке в любом порядке и комбинациях
-Пример работы shmoment(new Date(2020, 0, 7, 17, 17, 17)).add('minutes', 2).add('days', 8).subtract('years', 1).result(); // ... Jan 15 2019 17:19:17 ...
-Начальная дата должна остаться неизменной */
+Экспортируйте из файла ф-цию maxFibonacci, которая принимает аргументом число больше нуля
+Ф-ция maxFibonacci должна вернуть наибольшее число Фибоначчи, которое не больше заданного в аргументе
+Пример работы maxFibonacci(15); // 13 */
+
+
+
+
+// function maxFibonacci (number) {
+//     let index = 0;
+//     let maxNum = 0;
+
+//     while(true) {
+//         let num = index;
+//         if (num < number) {
+//             maxNum = num;
+//             index++;
+
+//         } else {
+//             return maxNum;
+//         }
+//     }
+// };
+
+// console.log(maxFibonacci(15));
+
+ const maxFibonacci = (number) => {
+    if (number < 1) return 0;
+    let prevNum = 1;
+    let secondNum = 1;
+    for (let i = 0; i <= number; i++) {
+        let numFib = prevNum + secondNum;
+        prevNum = secondNum;
+        secondNum = numFib;
+        if (numFib > number) return prevNum
+        if (numFib == number) return secondNum
+    }
+}
+
+console.log(maxFibonacci(15));
