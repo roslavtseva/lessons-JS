63ce40060654247633e2cc0927246b66b9236245
Macbook Pro <nastyarosl@gmail.com>
2020-01-21 18:16:47 +0200

    sol

--- less29/task2/index.js
+++ less29/task2/index.js
@@ -2,6 +2,7 @@
 
 export const pinger = (num, period) => {
     let i = num;
+    console.log('Ping');
    const interval =  setInterval(() => {
        if (i-- > 0){
            console.log('Ping');
