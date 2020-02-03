246ba5de0027e36abc429ac37cefb9c4d106b7a4
Macbook Pro <nastyarosl@gmail.com>
2020-02-03 16:33:35 +0200

    sol

--- less35/task1/index.js
+++ less35/task1/index.js
@@ -5,7 +5,7 @@ window.addEventListener("error", function onUnhandledError (err) {
 let userParsingResult;
 
 try {
-    const user = JSON.parse('{"name" : "Tom"}');
+    const user = JSON.parse('{"name" : "Tom"}'); //валідний
     console.log("User data: ", user);
     userParsingResult = "success";
 } catch(e) {
@@ -17,7 +17,7 @@ try {
 let productParsingResult;
 
 try {
-    const product = JSON.parse('{"name" : "Tom}');
+    const product = JSON.parse('{"name" : "Tom}'); //не валідний(без ")
     console.log("Product data: ", product);
     productParsingResult = "success";
 } catch(e) {
