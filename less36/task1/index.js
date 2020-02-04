cd3c1076395fc55b1fac43f64059883610525fc2
Macbook Pro <nastyarosl@gmail.com>
2020-02-04 11:24:40 +0200

    sol

---
+++ less36/task1/index.js
@@ -0,0 +1,17 @@
+const getUser = async userId => {
+    try {
+        const response = await fetch (`https://api.github.com/users/${userId}`); //запрос на сайт 
+        if(!response.ok){ //якщо корист не знайден то це значення буде false
+            return null; // тому вертаєм налл
+        } 
+        //витягуєм дані за сервера з доп json 
+        const userData = await response.json(); //з евейт отрим дані проміса а не сам проміс
+        return userData;
+    } catch (err) {
+        throw new Error('Failed to fetch user');
+    }
+    };
+
+getUser('facebook') //викл з айді користувача
+    .then(userData => console.log(userData))
+    .catch(err => alert(err.message));
