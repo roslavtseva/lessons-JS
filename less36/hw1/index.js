5878d4b97b56e248b560032885d9cd7c26aaed8b
Macbook Pro <nastyarosl@gmail.com>
2020-02-04 16:34:46 +0200

    sol

---
+++ less36/hw1/index.js
@@ -0,0 +1,16 @@
+  
+async function getUsersBlogs(users) {
+    const requests = users
+        .map(userId => fetch(`https://api.github.com/users/${userId}`)
+            .then(response => {
+                if (response.ok) return response.json();
+                throw new Error('Failed to load data');
+            }));
+    const usersData = await Promise.all(requests);
+    return usersData.map(user => user.blog);
+};
+
+//export { getUsersBlogs };
+
+getUsersBlogs(["facebook", "google"])
+.then(response => console.log(response))
